e261982bea3c3790537654d2d7c0a0e54e3fcd47
Gabriel Malafaia <gabrielbsnsp@gmail.com>
2023-04-03 11:49:07 -0300

    Update index.ts

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL: "https://collectionlinks.onrender.com",
+  baseURL: "https://node-js-collection-links-back-end.vercel.app/",
 });
 
 export default Api;
